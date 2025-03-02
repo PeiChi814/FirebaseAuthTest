b0baa60800ccd5d074b4246b37b242bd192b6143
PEICHI <qooqoo814@gmail.com>
2025-03-02 23:27:33 +0800

    Connect Firebase to the APP

--- FirebaseAuthTest/FirebaseAuthTestApp.swift
+++ FirebaseAuthTest/FirebaseAuthTestApp.swift
@@ -6,9 +6,22 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseAuth
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    print ("Firebae configured")
+    Auth.auth().useEmulator(withHost:"localhost", port:9099)
+    return true
+  }
+}
 
 @main
 struct FirebaseAuthTestApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
